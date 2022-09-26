74a0ee73e0aa5c2ce1c4f6f53bf9a68a8b44a422
xushiyin <yuqingxushiyin@gmail.com>
2022-09-26 09:12:44 +0800

    mount commands

--- cmd/mount.go
+++ cmd/mount.go
@@ -10,30 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// mountCmd represents the mount command
-var mountCmd = &cobra.Command{
-	Use:   "mount",
-	Short: "mount samba into local path",
-	Long: `mount samba share path into local path, or umount samba share path
-support Mac, Windows.
-Mac command line:
-	mount -t smbfs '//user:password@ip/share' /target/share
-	umount /target/share
-Windows command line:
-	net use X: \\ip\share /user:smb password
-	net use X: /delete
-
-for instance:
-	ndr mount       // mount samba share path into local path
-	ndr mount -d    //umount samba share path
-`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mount called")
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(mountCmd)
+	rootCmd.AddCommand(umountCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -44,4 +23,42 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// mountCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// mountCmd mount samba share path to local filesystem
+var mountCmd = &cobra.Command{
+	Use:   "mount",
+	Short: "mount samba into local path",
+	Long: `mount samba share path into local path, or umount samba share path
+support Mac, Windows.
+Mac command line:
+	mount -t smbfs '//user:password@ip/share' /target/share
+Windows command line:
+	net use X: \\ip\share /user:smb password
+
+for instance:
+	ndr mount       // mount samba share path into local path
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("mount called")
+	},
+}
+
+// umountCmd umount samba share path
+var umountCmd = &cobra.Command{
+	Use:   "umount",
+	Short: "umount samba share path",
+	Long: `umount samba share path
+support Mac, Windows.
+Mac command line:
+	umount /target/share
+Windows command line:
+	net use X: /delete
+
+for instance:
+	ndr umount     //umount samba share path
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("mount called")
+	},
 }
---
+++ internal/commands/commands.go
@@ -0,0 +1,5 @@
+// Package commands
+// Time    : 2022/9/25 23:05
+// Author  : xushiyin
+// contact : yuqingxushiyin@gmail.com
+package commands
